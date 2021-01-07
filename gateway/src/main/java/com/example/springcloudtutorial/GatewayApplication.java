2a7600537aca37f26c2cd5def0b5969f2ba4dc09
tomoya92 <py2qiuse@gmail.com>
2021-01-07 18:06:50 +0800

    gateway

--- gateway/src/main/java/com/example/springcloudtutorial/GatewayApplication.java
+++ gateway/src/main/java/com/example/springcloudtutorial/GatewayApplication.java
@@ -1,12 +1,45 @@
 package com.example.springcloudtutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
+
+    private Logger log = LoggerFactory.getLogger(GatewayApplication.class);
+
+    @Bean
+    public GlobalFilter customFilter() {
+        return new CustomGlobalFilter();
+    }
+
+    public class CustomGlobalFilter implements GlobalFilter, Ordered {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            log.info("custom global filter");
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            return chain.filter(exchange);
+        }
+
+        @Override
+        public int getOrder() {
+            return -1;
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
